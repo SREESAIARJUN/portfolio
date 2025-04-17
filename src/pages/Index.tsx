9e813a41aba670f1eea9a4978b1e7bb5f23abbd6
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2025-04-17 11:41:40 +0530

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,38 @@
+
+import React, { useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Experience from "../components/Experience";
+import Projects from "../components/Projects";
+import Publications from "../components/Publications";
+import Skills from "../components/Skills";
+import Achievements from "../components/Achievements";
+import Certifications from "../components/Certifications";
+import Footer from "../components/Footer";
+import AnimatedCursor from "../components/AnimatedCursor";
+
+const Index = () => {
+  useEffect(() => {
+    // Update the page title
+    document.title = "Kosinepalli Arjun Sai | AI/ML Portfolio";
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <AnimatedCursor />
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Publications />
+      <Skills />
+      <Achievements />
+      <Certifications />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
